aaf7193cb318fc16909c195d9c924a6eb4558b30
Chris PMaag <chrispmaag@gmail.com>
2021-01-26 23:53:57 -0800

    feat: Add PUT request for calling prediction with payload

--- main.py
+++ main.py
@@ -3,22 +3,34 @@
 # https://towardsdatascience.com/deployment-could-be-easy-a-data-scientists-guide-to-deploy-an-image-detection-fastapi-api-using-329cdd80400
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
+from pydantic import BaseModel
+
+class Input(BaseModel):
+    age: int
+    sex: str
 
 app = FastAPI()
 
-@app.post("/predict/image")
-async def predict_api(file: UploadFile = File(...)):
-    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
-    if not extension:
-        return "Image must be jpg or png format."
-    image = read_imagefile(await file.read())
-    prediction = predict(image)
 
-    return prediction
+# @app.post("/predict/image")
+# async def predict_api(file: UploadFile = File(...)):
+#     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+#     if not extension:
+#         return "Image must be jpg or png format."
+#     image = read_imagefile(await file.read())
+#     prediction = predict(image)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+#     return prediction
 
-if __name__ == "__main__":
-    uvicorn.run(app, debug=True)
+# Get request version
+# @app.get("/predict")
+# def predict_model(age: int, sex:str):
+#     if age < 10 or sex=='F':
+#         return {'survived': 1}
+#     return {'survived': 0}
+
+@app.put("/predict")
+def predict_model(d: Input):
+    if d.age < 10 or d.sex=='F':
+        return {'survived': 1}
+    return {'survived': 0}
